f673898870d442d9b5cedb6b275af5eccebc3e64
meyassu <51754935+meyassu@users.noreply.github.com>
2019-06-19 11:31:26 -0400

    Hero class

---
+++ Hero.java
@@ -0,0 +1,19 @@
+public abstract class Hero{
+
+   private String name;
+   private int size; 
+   private int speed;
+   private int health;
+   
+   public int injured(int damage){
+      health -= damage;
+      return health;
+   }
+   public int hit(int damage){
+      //Villain.getHealth() -= damage;
+      return damage;
+   }
+   public boolean move(String direction);
+   public boolean jump(boolean doubleJump);
+   
+   }
